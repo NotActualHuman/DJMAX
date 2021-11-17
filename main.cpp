b7462f95ef2d4fd2e14ba56342393b58c3e5c14a
NotActualHuman <67008111+NotActualHuman@users.noreply.github.com>
2021-11-17 16:11:20 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!";
+}
+
+// Why am I cheater? Anything you have actual proof?
+// If you have, send to nothingyoucanproof@protonmail.com
+// or If nothing you can proof I cheated in "DJMAX RESPECT V", You are fucking disgusting liar.
